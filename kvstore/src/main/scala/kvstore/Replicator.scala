f8738cc374b3895cf46df40a4f6aa2ef2ef8143e
Antonio Rodriguez <rodant68@gmail.com>
2013-12-27 19:17:32 +0100

    step 3 done

--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,9 +1,9 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor.{Cancellable, Props, Actor, ActorRef}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+import akka.util.Timeout
 
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
@@ -35,10 +35,33 @@ class Replicator(val replica: ActorRef) extends Actor {
     _seqCounter += 1
     ret
   }
+
+  def sendSnapshot(req: Replicator.Replicate, seq: Long) {
+    replica ! Snapshot(req.key, req.valueOption, seq)
+    context.system.scheduler.scheduleOnce(100 millis, self, seq)
+  }
   
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case req: Replicate => {
+      val seq = nextSeq
+      acks += seq -> (sender, req)
+      sendSnapshot(req, seq)
+    }
+    case SnapshotAck(key, seq) => {
+      val senderRequest: Option[(ActorRef, Replicate)] = acks.get(seq)
+      senderRequest match {
+        case Some((sender, req)) => {
+          acks -= seq
+          sender ! Replicated(key, req.id)
+        }
+        case None =>
+      }
+    }
+    case seq: Long if acks contains(seq) => {
+      val req = acks(seq)._2
+      sendSnapshot(req, seq)
+    }
   }
 
 }
