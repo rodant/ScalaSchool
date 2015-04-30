9477f1e5b8f3abbb906cf2a0275b807e8220bc5a
Antonio Rodriguez <rodant68@gmail.com>
2015-04-30 23:19:21 +0200

    polynomial roots implemented.

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,15 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b()*b()-4*a()*c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+      if (delta() > 0) Set((- b() + Math.sqrt(delta()))/(2*a()), (- b() - Math.sqrt(delta()))/(2*a()))
+      else if (delta() == 0) Set(- b()/2*a())
+      else Set()
+    }
   }
 }
